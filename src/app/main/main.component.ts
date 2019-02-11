92143a78765de14fe9ee1b3e436c406a3d28e115
Andy Pavliuk <andypavliuk11@gmail.com>
2019-02-11 20:46:53 +0200

    Update main.component.ts

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -10,16 +10,22 @@ export class MainComponent implements OnInit {
   //   { title: "TestPost", post: " Testing blog", author: "Bill" }
   // ];
 
-  PostArray: [{ title: string; post: string; author: string }] = [];
+  PostArray: [{ title: string; post: string; author: string; key:number }] = [];
 
-  addPost(event: Event){
-    let object = {title: this.title, post: this.post, author:this.author};
+  title = '';
+  post = '';
+  author = '';
+  key = Date.now();
+
+  addPost(event: Event) {
+    let object = { title: this.title, post: this.post, author: this.author, key: this.key };
     this.PostArray.push(object);
+    console.log(this.key);
+    this.title = null;
+    this.post = null;
+    this.author = null;
+    this.key = Date.now();
   }
-
-  title = null;
-  post = null;
-  author = null;
 
   constructor() {}
 
