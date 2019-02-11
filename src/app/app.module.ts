1e8a2fe43d2b09169ebfe301815ac2103b000af8
Andy Pavliuk <andypavliuk11@gmail.com>
2019-02-11 20:30:02 +0200

    Adding items to list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
-import { HeadComponent } from './head/head.component';
+import { HeadComponent } from './main/head/head.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { HeadComponent } from './head/head.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  selector: "app-main",
+  templateUrl: "./main.component.html",
+  styleUrls: ["./main.component.css"]
 })
 export class MainComponent implements OnInit {
+  // PostArray: [{ title: string; post: string; author: string }] = [
+  //   { title: "TestPost", post: " Testing blog", author: "Bill" }
+  // ];
 
-  constructor() { }
+  PostArray: [{ title: string; post: string; author: string }] = [];
 
-  ngOnInit() {
+  addPost(event: Event){
+    let object = {title: this.title, post: this.post, author:this.author};
+    this.PostArray.push(object);
   }
 
+  title = null;
+  post = null;
+  author = null;
+
+  constructor() {}
+
+  ngOnInit() {}
 }
