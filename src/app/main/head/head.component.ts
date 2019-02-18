69b328fe131a67e26899a786a75faa75ea825e7f
Andy Pavliuk <andypavliuk11@gmail.com>
2019-02-18 20:30:09 +0200

    Adding lifecircle props

--- src/app/main/head/head.component.ts
+++ src/app/main/head/head.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit, Input} from "@angular/core";
+import {
+  Component,
+  OnInit,
+  Input,
+  DoCheck,
+  OnChanges,
+  SimpleChanges,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+} from "@angular/core";
 
 @Component({
   selector: "app-head",
   templateUrl: "./head.component.html",
   styleUrls: ["./head.component.css"]
 })
-export class HeadComponent implements OnInit {
-  @Input() Post: [{ title: string; post: string; author: string, key: number }] = [{ title: 'TEST', post: 'TEST2', author: 'TEST3', key: Date.now() }];
+export class HeadComponent
+  implements
+    OnInit,
+    DoCheck,
+    OnChanges,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy {
+  @Input() Post: [
+    { title: string; post: string; author: string; key: number }
+  ] = [{ title: "TEST", post: "TEST2", author: "TEST3", key: Date.now() }];
 
-  
-  constructor() {}
+  constructor() {
+    console.log("constructor");
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log("NG ON INIT");
+  }
+
+  ngDoCheck() {
+    console.log("NG DO CHECK");
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log("NG ON CHANGES", changes);
+  }
+
+  ngAfterContentInit() {
+    console.log("NG AFTER CONTENT INIT");
+  }
+
+  ngAfterContentChecked() {
+    console.log("NG AFTER CONTENT CHECKED");
+  }
+
+  ngAfterViewInit() {
+    console.log("NG AFTER VIEW INIT");
+  }
+
+  ngAfterViewChecked() {
+    console.log("NG AFTER VIEW CHECKED");
+  }
+
+  ngOnDestroy() {
+    console.log("NG ON DESTROY");
+  }
 }
